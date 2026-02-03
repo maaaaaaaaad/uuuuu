a975c678e15125e3a449bb935161a6424e8bdf8c
maaaaaaaaad <integral5137@gmail.com>
2026-02-03 19:20:12 +0900

    add: location and distance fields to RecentShop entity

--- lib/features/recent_shops/domain/entities/recent_shop.dart
+++ lib/features/recent_shops/domain/entities/recent_shop.dart
@@ -7,6 +7,9 @@ class RecentShop extends Equatable {
   final String? address;
   final double? rating;
   final DateTime viewedAt;
+  final double? latitude;
+  final double? longitude;
+  final double? distance;
 
   const RecentShop({
     required this.shopId,
@@ -15,7 +18,39 @@ class RecentShop extends Equatable {
     this.address,
     this.rating,
     required this.viewedAt,
+    this.latitude,
+    this.longitude,
+    this.distance,
   });
+
+  RecentShop copyWith({
+    String? shopId,
+    String? shopName,
+    String? thumbnailUrl,
+    String? address,
+    double? rating,
+    DateTime? viewedAt,
+    double? latitude,
+    double? longitude,
+    double? distance,
+  }) {
+    return RecentShop(
+      shopId: shopId ?? this.shopId,
+      shopName: shopName ?? this.shopName,
+      thumbnailUrl: thumbnailUrl ?? this.thumbnailUrl,
+      address: address ?? this.address,
+      rating: rating ?? this.rating,
+      viewedAt: viewedAt ?? this.viewedAt,
+      latitude: latitude ?? this.latitude,
+      longitude: longitude ?? this.longitude,
+      distance: distance ?? this.distance,
+    );
+  }
+
+  String? get formattedDistance {
+    if (distance == null) return null;
+    return '${distance!.toStringAsFixed(1)}km';
+  }
 
   @override
   List<Object?> get props => [
@@ -25,5 +60,8 @@ class RecentShop extends Equatable {
         address,
         rating,
         viewedAt,
+        latitude,
+        longitude,
+        distance,
       ];
 }
