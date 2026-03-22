db36f240dc312b7e5b15b52854c6ebc620e863cf
maaaaaaaaad <integral5137@gmail.com>
2026-03-22 18:50:14 +0900

    refactor: ApiErrorHandler 적용 (usage_history)

--- lib/features/usage_history/data/repositories/usage_history_repository_impl.dart
+++ lib/features/usage_history/data/repositories/usage_history_repository_impl.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:jellomark/core/error/failure.dart';
+import 'package:jellomark/core/network/api_error_handler.dart';
 import 'package:jellomark/features/usage_history/data/datasources/usage_history_remote_datasource.dart';
 import 'package:jellomark/features/usage_history/domain/entities/usage_history.dart';
 import 'package:jellomark/features/usage_history/domain/repositories/usage_history_repository.dart';
@@ -16,15 +17,9 @@ class UsageHistoryRepositoryImpl implements UsageHistoryRepository {
       final models = await remoteDataSource.getMyUsageHistory();
       return Right(models.map((m) => m.toEntity()).toList());
     } on DioException catch (e) {
-      return Left(ServerFailure(_getErrorMessage(e)));
+      return Left(
+        ApiErrorHandler.fromDioException(e, fallback: '이용 내역을 불러올 수 없습니다'),
+      );
     }
-  }
-
-  String _getErrorMessage(DioException e) {
-    final data = e.response?.data;
-    if (data is Map<String, dynamic> && data.containsKey('message')) {
-      return data['message'] as String;
-    }
-    return '서버 오류가 발생했습니다';
   }
 }
