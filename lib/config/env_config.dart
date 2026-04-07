1df5027c2c77795d49851b0e180b7280e92a0455
maaaaaaaaad <integral5137@gmail.com>
2026-04-07 12:41:19 +0900

    refactor: bump version to 1.0.0+38

--- lib/config/env_config.dart
+++ lib/config/env_config.dart
@@ -6,7 +6,7 @@ class EnvConfig {
 
   static const String apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://api.jellomark.com',
+    defaultValue: 'https://api.jellomark.com',
   );
 
   static const String kakaoNativeAppKey = String.fromEnvironment(
