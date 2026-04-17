4cca0296f5f48b5cd484cb9bb004462ff0dc3c52
maaaaaaaaad <integral5137@gmail.com>
2026-04-17 12:19:49 +0900

    refactor: increase marker size to 40px in shop detail map

--- lib/features/beautishop/presentation/widgets/shop_map_widget.dart
+++ lib/features/beautishop/presentation/widgets/shop_map_widget.dart
@@ -45,12 +45,12 @@ class _ShopMapWidgetState extends State<ShopMapWidget> {
   NOverlayImage? _shopIcon;
   NOverlayImage? _userIcon;
 
-  static const _markerSize = Size(34, 34);
+  static const _markerSize = Size(40, 40);
 
   Widget _buildCircleMarker({required Decoration decoration}) {
     return Container(
       decoration: decoration,
-      child: const Icon(Icons.place, color: Colors.white, size: 17),
+      child: const Icon(Icons.place, color: Colors.white, size: 20),
     );
   }
 
